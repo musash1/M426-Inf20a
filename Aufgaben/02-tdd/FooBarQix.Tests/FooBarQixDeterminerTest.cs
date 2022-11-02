9016d3b406dccce0dc276ef39ce38ae931d9d412
ruuiisu <luis.gjokaj@gmail.com>
2022-11-02 13:47:42 +0100

    Aufgaben 2-TDD: Alle Funktionen und Tests umgesetzt.

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,101 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void testIfFoo()
         {
+            int a = 9;
+            string expected = "Foo";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testIfBar()
+        {
+            int a = 10;
+            string expected = "Bar";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testIfQix()
+        {
+            int a = 14;
+            string expected = "Qix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testIfFooBar()
+        {
+            int a = 15;
+            string expected = "FooBar";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testIfFooQix()
+        {
+            int a = 21;
+            string expected = "FooQix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testIfBarQix()
+        {
+            int a = 35;
+            string expected = "BarQix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testIfFooBarQix()
+        {
+            int a = 105;
+            string expected = "FooBarQix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            // Act
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,32 @@
-﻿namespace FooBarQix
+﻿using System.Diagnostics;
+
+namespace FooBarQix
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int input)
+        {
+            string result = "";
 
+            if (input % 3 == 0)
+            {
+                result += "Foo";
+            }
+            if (input % 5 == 0)
+            {
+                result += "Bar";
+            }
+            if (input % 7 == 0)
+            {
+                result += "Qix";
+            }
+            if (result == "")
+            {
+                result = input.ToString();
+            }
+
+            Debug.WriteLine(result);
+            return result;
+        }
     }
 }
